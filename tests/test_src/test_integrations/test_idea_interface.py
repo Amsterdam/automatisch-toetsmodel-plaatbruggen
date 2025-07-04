34bafd57a0ced5329ae8601127f772e8d9d61272
RahmanOzdemir <rahman.ozdemir@arcadis.com>
2025-07-04 16:17:24 +0200

    removed idea tests

--- tests/test_src/test_integrations/test_idea_interface.py
+++ tests/test_src/test_integrations/test_idea_interface.py
@@ -7,17 +7,5 @@ including model creation, parameter extraction, and analysis functionality.
 
 import pytest
 
-from src.integrations import idea_interface
-
-
-def test_calculate_rebar_positions_even():
-    """Test even number of rebars are placed symmetrically."""
-    positions = idea_interface.calculate_rebar_positions(1000, 200)
-    # Should create 5 rebars: positions should be symmetric and spaced
-    assert isinstance(positions, list)
-    assert len(positions) == 5
-    assert positions[0] < 0 < positions[-1]
-
-
 if __name__ == "__main__":
     pytest.main([__file__])
