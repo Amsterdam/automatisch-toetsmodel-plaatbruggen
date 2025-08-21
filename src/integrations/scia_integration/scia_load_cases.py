8c38003199fa7aafc48326032d460fb267557ee8
Paul Wensveen <p.wensveen@amsterdam.nl>
2025-08-21 14:45:53 +0200

    removed BG4003 in scia_load_cases

--- src/integrations/scia_integration/scia_load_cases.py
+++ src/integrations/scia_integration/scia_load_cases.py
@@ -141,8 +141,7 @@ def create_udl_traffic_load_cases(builder: SciaModelBuilder) -> dict[str, SciaLo
     data = [
         ("rs_1", "BG4001", "Verkeer, dek - LM1 UDL RS 1"),
         ("rs_2", "BG4002", "Verkeer, dek - LM1 UDL RS 2"),
-        ("rs_3", "BG4003", "Verkeer, dek - LM1 UDL RS 3"),
-        ("rest", "BG4004", "Verkeer, dek - LM1 UDL rest"),
+        ("rs_3", "BG4003", "Verkeer, dek - LM1 UDL RS 3")
     ]
     cases = {}
     for key, name, desc in data:
