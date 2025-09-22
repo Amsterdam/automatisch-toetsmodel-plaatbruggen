92b92787b73dd223e235b2c4907e2af3d6c848f3
Geert Vos <geert.vos@amsterdam.nl>
2025-09-22 15:17:49 +0200

    Auto-format code with ruff (147 files reformatted)

--- app/overview_bridges/parametrization.py
+++ app/overview_bridges/parametrization.py
@@ -17,8 +17,10 @@ class OverviewBridgesParametrization(Parametrization):
 
     # Define the Bridge Overview page
     bridge_overview = Page("Overzicht Bruggen", views=["get_map_view"])
-    bridge_overview.introduction = Text("Op deze pagina vind je een overzicht van alle plaatbruggen in het project."
-    " Je kunt hier nieuwe bruggen toevoegen of bestaande bruggen bewerken of verwijderen. Klik op een brug in de lijst om naar de brug te gaan.")
+    bridge_overview.introduction = Text(
+        "Op deze pagina vind je een overzicht van alle plaatbruggen in het project."
+        " Je kunt hier nieuwe bruggen toevoegen of bestaande bruggen bewerken of verwijderen. Klik op een brug in de lijst om naar de brug te gaan."
+    )
 
     # ChildEntityManager linked by passing the registered entity_type_name (alias)
     bridge_overview.bridge_manager = ChildEntityManager("Bridge")
